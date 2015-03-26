3be39ab829b956de8b1b33156d9104de0db876f2
Mary Bucknell <mbucknell@usgs.gov>
2015-03-26 08:30:39 -0500

    Brought in image directory. Added visual page header to the base template.

--- portal_ui/views.py
+++ portal_ui/views.py
@@ -3,6 +3,7 @@ from flask import render_template
 
 from . import app
 
+@app.route('/index.jsp')
 @app.route('/index/')
-def index():
+def home():
     return render_template('index.html', author='Mary')
