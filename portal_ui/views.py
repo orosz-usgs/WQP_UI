90784e6f1bc15883b01b65982cce9f950a5f8f3d
Mary Bucknell <mbucknell@usgs.gov>
2015-03-31 15:51:17 -0500

    Added faqs

--- portal_ui/views.py
+++ portal_ui/views.py
@@ -34,6 +34,13 @@ def webservices_documentation():
     return render_template('webservices_documentation.html', feed_content=pull_feed(feed_url))
 
 
+@app.route('/faqs.jsp')
+@app.route('/faqs/')
+def faqs():
+    feed_url = "https://my.usgs.gov/confluence/createrssfeed.action?types=page&spaces=qwdp&title=myUSGS+4.0+RSS+Feed&labelString=wqp_faqs&excludedSpaceKeys%3D&sort=modified&maxResults=1&timeSpan=3650&showContent=true&confirm=Create+RSS+Feed"
+    return render_template('faqs.html', feed_content=pull_feed(feed_url))
+
+
 @app.route('/coverage.jsp')
 @app.route('/coverage/')
 def coverage():
