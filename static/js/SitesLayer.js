953e7d2ff86b0a0d3eef12be3a639c335eb783c0
Tomcat Application Server <tomcat@cida-eros-wqpdev.er.usgs.gov>
2016-02-08 11:57:19 -0600

    automatic inclusion of the env, static files, and generated assets

---
+++ static/js/SitesLayer.js
@@ -0,0 +1,119 @@
+/*
+ * Provides methods to create a site layer and to add an id control
+ * @param {Array of Objects with name and value properties} queryParamArray
+ * @param {Function} loadendCallback - to be called when loadend event is triggered.
+ * @param {Boolean} enableBoxId
+ * @param {Function} selectFeatureFnc - function to execute when selecting features on the layer
+ * @returns {Object}
+ */
+
+function SitesLayer(queryParamArray,
+					loadendCallback,
+					enableBoxId,
+					selectFeatureFnc) {
+	this._isBoxIDEnabled = enableBoxId;
+	this._selectFeatureFnc = selectFeatureFnc;
+
+	var getSearchParams = function (queryParamArray) {
+		var queryString = PORTAL.UTILS.getQueryString(queryParamArray, ['mimeType', 'zip'], true);
+		var result = decodeURIComponent(queryString.replace(/\+/g, '%20'))
+		return result.replace(/=/g, ':').replace(/;/g, '|').replace(/&/g, ';');
+	};
+
+	this.searchParams = getSearchParams(queryParamArray);
+
+	this.dataLayer = new OpenLayers.Layer.WMS(
+		'Sites',
+		Config.SITES_GEOSERVER_ENDPOINT + 'wms',
+		{
+			layers: 'wqp_sites',
+			styles: 'wqp_sources',
+			format: 'image/png',
+			transparent: true,
+			searchParams: this.searchParams
+		},
+		{
+			displayInLayerSwitcher: false,
+			isBaseLayer: false,
+			transitionEffect: 'resize',
+			singleTile: true,
+			visibility: true,
+			opacity: 0.75,
+			tileOptions: {
+				// http://www.faqs.org/rfcs/rfc2616.html
+				// This will cause any request larger than this many characters to be a POST
+				maxGetUrlLength: 1024
+			}
+
+		}
+	);
+	this.dataLayer.events.register('loadend', this, function (ev) {
+		loadendCallback();
+	});
+
+	this._createIdControl = function () {
+		var filter = new OpenLayers.Filter.Comparison({
+			type: OpenLayers.Filter.Comparison.EQUAL_TO,
+			property: 'searchParams',
+			value: encodeURIComponent(this.searchParams)
+		});
+		var protocol = new OpenLayers.Protocol.WFS({
+			version: '1.1.0',
+			url: Config.SITES_GEOSERVER_ENDPOINT + 'wfs',
+			srsName: 'EPSG:900913',
+			featureType: 'wqp_sites',
+			featureNS: '',
+			featurePrefix: '',
+			defaultFilter: filter
+		});
+
+		//var protocol = OpenLayers.Protocol.WFS.fromWMSLayer(this.dataLayer);
+		this.idFeatureControl = new WQPGetFeature({
+				protocol: protocol,
+				box: this._isBoxIDEnabled,
+				click: !this._isBoxIDEnabled,
+				clickTolerance: 5,
+				single: false,
+				maxFeatures: 50
+			}
+		);
+
+		this.idFeatureControl.events.register('featuresselected', this, function (ev) {
+			this._selectFeatureFnc(ev, this.idFeatureControl.getLastBoundingBox());
+		});
+		this.idFeatureControl.events.register('clickout', this, function (ev) {
+			console.log('No feature selected');
+		});
+	};
+
+	this.idFeatureControl = null;
+
+	this.addToMap = function (map /* OpenLayers map object */) {
+		map.addLayer(this.dataLayer);
+
+		this._createIdControl();
+		map.addControl(this.idFeatureControl);
+		this.idFeatureControl.activate();
+	};
+
+	this.removeFromMap = function (map /* OpenLayers map object */) {
+		this.idFeatureControl.deactivate();
+		map.removeControl(this.idFeatureControl);
+		map.removeLayer(this.dataLayer);
+	};
+
+	this.enableBoxId = function (map /* OpenLayer map object */, on /* Boolean */) {
+
+		if (this._isBoxIDEnabled !== on) {
+			this._isBoxIDEnabled = on;
+
+			this.idFeatureControl.deactivate();
+			map.removeControl(this.idFeatureControl);
+
+			this._createIdControl();
+
+			map.addControl(this.idFeatureControl);
+			this.idFeatureControl.activate();
+		}
+	};
+};
---
+++ static/js/views/siteParameterInputView.js
@@ -0,0 +1,88 @@
+/* jslint browser: true */
+/* global $ */
+
+var PORTAL = PORTAL || {};
+PORTAL.VIEWS = PORTAL.VIEWS || {};
+
+/*
+ * Creates a site parameter input view object
+ * @param {Object} options
+ * 		@prop {Jquery element} $container - element where the site parameter inputs are contained
+ * 		@prop {PORTAL.MODELS.cachedCodes} siteTypeModel
+ * 		@prop {PORTAL.MODELS.cachedCodes} organizationModel
+ * @returns {Object}
+ * 		@func initialize;
+ */
+PORTAL.VIEWS.siteParameterInputView = function(options) {
+	"use strict";
+
+	var self = {};
+
+	var initializeOrganizationSelect = function($select, model) {
+		var formatData = function(data) {
+			return {
+				id : data.id,
+				text : data.id + ' - ' + data.desc
+			};
+		};
+		var isMatch = function(searchTerm, data) {
+			var termMatcher;
+			if (searchTerm) {
+				termMatcher = new RegExp(searchTerm, 'i');
+				return (termMatcher.test(data.id) || termMatcher.test(data.desc));
+			}
+			else {
+				return true;
+			}
+		};
+		PORTAL.VIEWS.createCodeSelect($select, {
+			model : model,
+			formatData : formatData,
+			isMatch : isMatch
+		}, {
+			minimumInputLength: 2,
+			closeOnSelect : false
+		});
+	};
+
+	/*
+	 * Initialize the widgets and DOM event handlers
+	 * @return Jquery promise
+	 * 		@resolve - when all models have been fetched successfully
+	 * 	    @reject - if any model's fetch failed.
+	 */
+	self.initialize = function() {
+		var $siteTypeSelect = options.$container.find('#siteType');
+		var $organizationSelect = options.$container.find('#organization');
+		var $siteIdInput = options.$container.find('#siteid');
+		var $hucInput = options.$container.find('#huc');
+
+		var fetchSiteType = options.siteTypeModel.fetch()
+		var fetchOrganization = options.organizationModel.fetch()
+		var fetchComplete = $.when(fetchSiteType, fetchOrganization);
+
+		fetchSiteType.done(function() {
+			PORTAL.VIEWS.createCodeSelect($siteTypeSelect, {model : options.siteTypeModel});
+		});
+		fetchOrganization.done(function() {
+			initializeOrganizationSelect($organizationSelect, options.organizationModel);
+		});
+
+		// Add event handlers
+		PORTAL.VIEWS.inputValidation({
+			inputEl: $siteIdInput,
+			validationFnc: PORTAL.validators.siteIdValidator
+		});
+		PORTAL.VIEWS.inputValidation({
+			inputEl: $hucInput,
+			validationFnc: PORTAL.hucValidator.validate,
+			updateFnc: PORTAL.hucValidator.format
+		});
+
+		return fetchComplete;
+	};
+
+	return self;
+};
+
+
