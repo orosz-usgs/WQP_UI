9bcbd9f357e0d74d68278865826dd61743031103
Mary Bucknell <mbucknell@usgs.gov>
2016-02-22 11:41:39 -0600

    Create a new view, showAPIView which will initialize the part of the page which shows the various kinds of queries.

---
+++ portal_ui/static/js/views/showAPIView.js
@@ -0,0 +1,36 @@
+/* jslint browser: true */
+
+var PORTAL = PORTAL || {};
+
+PORTAL.VIEWS = PORTAL.VIEWS || {};
+
+/*
+ * Initializes the windows which show the various API calls
+ * @param {Object} options
+ * 		@prop {Jquery element} $container - The container containing the show button and the query windows.
+ * 		@prop {Function} getQueryParamArray - Returns the current query parameter array
+ 	* 		@returns {Array of Objects with name and value properties}
+ */
+PORTAL.VIEWS.showAPIView = function(options) {
+	"use strict";
+
+	var self = {};
+
+	self.initialize = function() {
+		var $apiQueryDiv = options.$container.find('#api-queries-div');
+		var $sitesText = options.$container.find('#sites-query-div textarea');
+		var $resultsText = options.$container.find('#results-query-div textarea');
+
+		options.$container.find('#show-queries-button').click(function() {
+			var queryString = PORTAL.UTILS.getQueryString(options.getQueryParamArray());
+
+			$apiQueryDiv.show();
+			$sitesText.html(PORTAL.queryServices.getFormUrl('Station', queryString));
+			$resultsText.html(PORTAL.queryServices.getFormUrl('Result', queryString));
+		});
+	}
+
+	return self;
+
+};
+
