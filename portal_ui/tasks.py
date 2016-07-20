3b49ba9b539729269f29587da6be05e5edeb45fd
Jim Kreft <jkreft@usgs.gov>
2016-07-20 14:36:21 -0500

    PEP8

--- portal_ui/tasks.py
+++ portal_ui/tasks.py
@@ -1,9 +1,10 @@
-from celery import Celery
-from . import app
-import requests
-import redis
 import csv
 import cPickle as pickle
+from celery import Celery
+import requests
+import redis
+from . import app
+
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
@@ -12,21 +13,21 @@ celery.conf.update(app.config)
 @celery.task(bind=True)
 def generate_site_list_from_streamed_tsv_async(self, base_url, redis_config, provider_id, redis_db):
     """
-
+    :param self: self
     :param base_url: the base url we are using for the generating the search URL
-    :param provider_id:
-    :param redis_config:
+    :param provider_id: the dientifier of the provider (NWIS, STORET, ETC)
+    :param redis_config: redis config variables
+    :param redis_db: the db number that we are pushing the cached info to
     :return: a list of dicts that describe sites that are associated with an organization under a data provider
     """
     search_endpoint = base_url + "Station/search/"
-    r = requests.get(search_endpoint, { "providers": provider_id,
+    r = requests.get(search_endpoint, {"providers": provider_id,
                                        "mimeType": "tsv", "sorted": "no", "uripage": "yes"}, stream=True
                      )
 
     status = r.status_code
     headers = r.headers
     total = int(headers['Total-Site-Count'])
-    header_line = None
     redis_session = None
     if redis_config:
         redis_session = redis.StrictRedis(host=redis_config['host'], port=redis_config['port'], db=redis_db,
@@ -35,6 +36,7 @@ def generate_site_list_from_streamed_tsv_async(self, base_url, redis_config, pro
     error_count = 0
     cached_count = 0
     counter = 0
+    header = None
     for line in r.iter_lines():
         # filter out keep-alive new lines
         if line:
@@ -48,7 +50,7 @@ def generate_site_list_from_streamed_tsv_async(self, base_url, redis_config, pro
                 station = csv.reader([line], delimiter='\t')
                 for row in station:
                     station_data = row
-                    if len(station_data) == 36:
+                    if len(station_data) == 36 and header is not None:
                         station_dict = dict(zip(header, station_data))
                         site_key = 'sites_' + provider_id + '_' + str(station_dict['OrganizationIdentifier']) + "_" + \
                                    str(station_dict['MonitoringLocationIdentifier'])
@@ -61,6 +63,5 @@ def generate_site_list_from_streamed_tsv_async(self, base_url, redis_config, pro
                         counter += 1
                     elif len(station_data) != 36:
                         error_count += 1
-
 
     return {"status": status, "cached_count": cached_count, "error_count": error_count}
