6338486e65c7bfba6db21b475e5c429139752f57
Yan, Andrew N <ayan@usgs.gov>
2016-09-21 13:24:41 -0500

    rework --clean behavior

--- dev_install.ps1
+++ dev_install.ps1
@@ -1,28 +1,36 @@
-param([String]$update="")
+param([String]$arg1="--update")
 
 Write-Output "You must have node, karma, Python 2.7, and virtualenv installed and available via the system path."
 
-$configExists = Test-Path instance/config.py
+$configExists = Test-Path instance\config.py
 if (-Not $configExists) {
-	Write-Output "Please create an instance/config.py file before proceeding. See the README.md for what's required."
+	Write-Output "Please create an instance\config.py file before proceeding. See the README.md for what's required."
 	Exit
 }
 
-if ($update -eq "--update") {
+if ($arg1 -eq "--update") {
 	Write-Output "Updating npm and bower dependencies"
 	npm update
 	bower update
 }
-else {
-	Write-Output "Installing npm and bower dependencies"
+
+if ($arg1 -eq "--clean") {
+	Write-Output "Clearing old dependencies."
+	Remove-Item node_modules -Force -Recurse
+	Remove-Item portal_ui\bower_components -Force -Recurse
+	Write-Output "Installing dependencies"
 	npm install
 	bower install
 }
 
 Write-Output "Running Javascript tests."
-karma start test/js/karma.conf.js
+node node_modules\karma\bin\karma start test\js\karma.conf.js
 
 Write-Output "Creating the virtualenv and installing Python requirements"
+if ($arg1 -eq "--clean") {
+	Remove-Item env -Force -Recurse
+}
+
 $envExists = Test-Path env
 if (-Not $envExists) {
 	Write-Output "Creating a virtualenv."
--- dev_install.sh
+++ dev_install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+ARG1=${1:-"--update"};
+
 echo "You must have python 2.7 and virtualenv installed"
 if [ ! -s instance/config.py ]; then
 	echo "Please create an instance/config.py file before proceeding. See the README.md for what's required."
 	return
 fi
 
-if [ "$1" == '--update' ]; then
+if [ "$ARG1" == '--update' ]; then
 	echo "Updating npm and bower dependencies";
 	npm update;
 	bower update;
-else
+fi
+
+if [ "$ARG1" == '--clean' ]; then
+	echo "Cleaning out current dependencies";
+	rm -rf node_modules;
+	rm -rf portal_ui/bower_components;
 	echo "Installing npm and bower dependencies";
 	npm install;
 	bower install;
@@ -21,6 +28,10 @@ node_modules/karma/bin/karma start test/js/karma.conf.js;
 
 
 echo "Creating the virtualenv env and installing python requirements"
+if [ "$ARG1" == "--clean" ]; then
+	rm -rf env;
+fi
+
 if [ ! -s env ]; then
 	virtualenv --python=python2.7 env
 fi
