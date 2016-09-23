64ba8b101e5f317105e8e8df8ebeaf0e09790873
Mary Bucknell <mbucknell@usgs.gov>
2016-09-23 11:46:22 -0500

    Updated the install scripts to consolidate the directory removal in one clause and add the static/.webassets-cache and static/gen directories.

--- dev_install.ps1
+++ dev_install.ps1
@@ -8,6 +8,16 @@ if (-Not $configExists) {
 	Exit
 }
 
+if ($arg1 -eq "--clean") {
+	Write-Output "Clearing old dependencies."
+	Remove-Item env -Force -Recurse
+	Remove-Item node_modules -Force -Recurse
+	Remove-Item portal_ui\bower_components -Force -Recurse
+	Remove-Item portal_ui/static/.webassets-cache -Force -Recurse
+	Remove-Item portal_ui/static/gen -Force -Recurse
+
+}
+
 if ($arg1 -eq "--update") {
 	Write-Output "Updating npm and bower dependencies"
 	npm update
@@ -15,10 +25,7 @@ if ($arg1 -eq "--update") {
 }
 
 if ($arg1 -eq "--clean") {
-	Write-Output "Clearing old dependencies."
-	Remove-Item node_modules -Force -Recurse
-	Remove-Item portal_ui\bower_components -Force -Recurse
-	Write-Output "Installing dependencies"
+	Write-Output "Installing npm and bower dependencies"
 	npm install
 	bower install
 }
@@ -28,7 +35,6 @@ node node_modules\karma\bin\karma start test\js\karma.conf.js
 
 Write-Output "Creating the virtualenv and installing Python requirements"
 if ($arg1 -eq "--clean") {
-	Remove-Item env -Force -Recurse
 }
 
 $envExists = Test-Path env
@@ -39,6 +45,7 @@ if (-Not $envExists) {
 else {
 	Write-Output "Virtualenv already exists."
 }
+Write-Output "Installing python requirements."
 env\Scripts\pip install -r requirements.txt
 
 Write-Output "Finished setting up WQP-UI."
