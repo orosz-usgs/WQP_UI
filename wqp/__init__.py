8637c72f7c4b4d489381f38adab12f37e2b8a3a9
Mary Bucknell <mbucknell@usgs.gov>
2016-10-06 12:09:43 -0500

    Using flask-swagger and flask-swagger-ui now. UI is more nicely formated. Will have to push to development server to see if there are any issues.

--- wqp/__init__.py
+++ wqp/__init__.py
@@ -1,8 +1,10 @@
 
 from celery import Celery
-from flasgger import Swagger
-from flask import Flask
+from flask import Flask, jsonify
 from flask_bower import Bower
+from flask_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
+
 
 app = Flask(__name__.split()[0], instance_relative_config=True)
 
@@ -16,11 +18,11 @@ app.config['SWAGGER'] = {
             'title' : 'NWIS site API',
             'description': 'Streaming NWIS Site service',
             'endpoint' : 'sites',
-            'route': '/api/'
+            'route': '/sites/'
         }
-    ]
+    ],
+    'url_prefix' : '/wsgi/wqp_ui'
 }
-Swagger(app)
 
 # Loads configuration information from config.py and instance/config.py
 app.config.from_object('config')
@@ -33,6 +35,29 @@ celery.conf.update(app.config)
 
 from portal_ui.views import portal_ui
 from sites.views import sites
+
 app.register_blueprint(portal_ui, url_prefix='')
 app.register_blueprint(sites,
                        url_prefix='/sites')
+
+# Create swagger ui blueprint
+SWAGGER_URL = '/apidocs'
+API_URL = '/spec'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+# Set up swagger endpoints
+@app.route('/spec')
+def spec():
+    return jsonify(swagger(app,
+                           from_file_keyword="swagger_from_file",
+                           template={
+                               "info": {
+                                   "version": "1.0",
+                                   "title": "WQP Sites service"
+                               }
+                           }))
