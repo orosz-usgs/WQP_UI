c51aad8b2d78aa7a9762749524f2994109833333
Mary Bucknell <mbucknell@usgs.gov>
2016-10-17 10:22:31 -0500

    remove unused import

--- wqp/__init__.py
+++ wqp/__init__.py
@@ -1,6 +1,6 @@
 
 from celery import Celery
-from flask import Flask, jsonify, request, url_for
+from flask import Flask, jsonify, request
 from flask_bower import Bower
 from flask_swagger import swagger
 from flask_swagger_ui import get_swaggerui_blueprint
