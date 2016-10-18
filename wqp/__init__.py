638e21fa7a7be48655d32d96b1c4b642d07bab92
Yan, Andrew N <ayan@usgs.gov>
2016-10-18 16:02:00 -0500

    correct path and start tests

--- wqp/__init__.py
+++ wqp/__init__.py
@@ -40,7 +40,7 @@ from wqx.views import wqx
 app.register_blueprint(portal_ui, url_prefix='')
 app.register_blueprint(sites,
                        url_prefix='/sites')
-app.register_blueprint(wqx, url_prefix='/schemas')
+app.register_blueprint(wqx, url_prefix='/portal/schemas')
 
 # Set up swagger endpoints
 @app.route('/spec')
