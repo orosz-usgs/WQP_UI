5b9f4027fa70c31eb3b6494b1fe55af50739b5c1
Yan, Andrew N <ayan@usgs.gov>
2016-11-16 15:55:46 -0600

    use sessions to persist SSL cert verification across multiple requests

--- wqp/__init__.py
+++ wqp/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request
 from flask_bower import Bower
 from flask_swagger import swagger
 from flask_swagger_ui import get_swaggerui_blueprint
+from requests import Session
 
 
 __version__ = '4.2.0dev'
@@ -22,9 +23,14 @@ import assets
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
+
+session = Session()
+session.verify = app.config.get('VERIFY_CERT', True)
+
 from portal_ui.views import portal_ui
 from sites.views import sites
 from wqx.views import wqx
+
 
 app.register_blueprint(portal_ui, url_prefix='')
 app.register_blueprint(sites,
