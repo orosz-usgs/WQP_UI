0e8188b25dde315b3c45cbc5d16c4165a4b325db
Mary Bucknell <mbucknell@usgs.gov>
2016-11-28 11:40:50 -0600

    Updated leafletUtils tests

--- test/js/portal_ui/leafletUtilsSpec.js
+++ test/js/portal_ui/leafletUtilsSpec.js
@@ -1,38 +1,15 @@
 /* jslint browser: true */
 /* global L */
+/* global WQP */
 /* global describe, it, expect */
 
 describe('leafletUtils', function() {
 	"use strict";
 
-	describe('Tests for getIntersection', function() {
+	describe('Tests for toBBoxString', function() {
 
-		it('Expects that if bounds1 is contained in bounds2 that the returned value is bounds1', function() {
-			var bounds1 = L.latLngBounds(L.latLng(41, -99), L.latLng(42, -98));
-			var bounds2 = L.latLngBounds(L.latLng(40, -100), L.latLng(43, -97));
-
-			expect(L.Util.getIntersection(bounds1, bounds2)).toEqual(bounds1);
-		});
-
-		it('Expects that if bounds2 is contained in bounds1 that the returned value is bounds2', function() {
-			var bounds2 = L.latLngBounds(L.latLng(41, -99), L.latLng(42, -98));
-			var bounds1 = L.latLngBounds(L.latLng(40, -100), L.latLng(43, -97));
-
-			expect(L.Util.getIntersection(bounds1, bounds2)).toEqual(bounds2);
-		});
-
-		it('Expects that if bounds1 and bounds 2 do not intersect return undefined', function() {
-			var bounds1 = L.latLngBounds(L.latLng(41, -99), L.latLng(42, -98));
-			var bounds2 = L.latLngBounds(L.latLng(43, -97), L.latLng(44, -96));
-
-			expect(L.Util.getIntersection(bounds1, bounds2)).toBeUndefined();
-		});
-
-		it('Expects that if bounds1 and bounds2 intersect that this returns the intersecting bounds', function() {
-			var bounds1 = L.latLngBounds(L.latLng(41, -99), L.latLng(43, -97));
-			var bounds2 = L.latLngBounds(L.latLng(42, -98), L.latLng(44, -96));
-
-			expect(L.Util.getIntersection(bounds1, bounds2)).toEqual(L.latLngBounds(L.latLng(42, -98), L.latLng(43, -97)));
+		it('Expects toBBoxString to return an formatted string representing the bounds', function() {
+			expect(WQP.L.Util.toBBoxString(L.latLngBounds(L.latLng(42, -99), L.latLng(43, -98)))).toEqual('42,-99,43,-98');
 		});
 	});
 });
--- wqp/portal_ui/static/js/leafletUtils.js
+++ wqp/portal_ui/static/js/leafletUtils.js
@@ -4,6 +4,10 @@ var WQP = WQP || {};
 WQP.L = WQP.L || {};
 WQP.L.Util = WQP.L.Util || {};
 
+/*
+ * @param {L.LatLngBounds} bounds
+ * @returns a string representing bounds as south,west,north,east
+ */
 WQP.L.Util.toBBoxString = function(bounds) {
 	"use strict";
 	return bounds.getSouth() + ',' + bounds.getWest() + ',' + bounds.getNorth() + ',' + bounds.getEast();
