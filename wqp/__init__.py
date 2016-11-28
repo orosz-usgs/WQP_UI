48ffb1eb07f8836f6bc7f82725f3c40c78693a7e
Jim Kreft <jkreft@usgs.gov>
2016-11-28 16:22:05 -0600

    change to after config is loaded

--- wqp/__init__.py
+++ wqp/__init__.py
@@ -13,10 +13,13 @@ __version__ = '4.3.0dev'
 app = Flask(__name__.split()[0], instance_relative_config=True)
 
 Bower(app)
-sslify = SSLify(app, permanent=True)
+
 # Loads configuration information from config.py and instance/config.py
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+# Force SSL
+sslify = SSLify(app, permanent=True)
 
 import assets
 
