1550b3f2db87c21abb110573a7c1da23dcbe751c
Mary Bucknell <mbucknell@usgs.gov>
2016-12-02 14:21:56 -0600

    Using Leaflet to render the coverage mapper. Still need to reimplement the click handler and dialog and loading indicator.

---
+++ wqp/portal_ui/static/js/coverage/coverageMap.js
@@ -0,0 +1,59 @@
+/* jslint browser: true */
+/* global L */
+
+var COVERAGE = COVERAGE || {};
+
+/*
+ * @param {Object} options
+ * 		@prop {String} mapDivId - Div where map will be rendered
+ * 	    @prop {Jquery element} $legendImg - image element which will contain the legend
+ * 		@prop {Object} layerParams -
+ * 	 		@prop {String} displayBy - spatial feature
+ *	 		@prop {String} timeSpan - Allowed values: past_12_months, past_60_months, all_time
+ * 			@prop {String} dataSource - Allowed values: storet, nwis, all.
+ */
+COVERAGE.coverageMap = function(options) {
+	"use strict";
+	var self = {};
+
+	var BASE_LAYER_Z_INDEX = 1;
+	var DATA_LAYER_Z_INDEX = 3;
+
+	var MapWithSingleClickHandler = L.Map.extend({
+		includes: L.SingleClickEventMixin
+	});
+
+
+	var baseLayers = {
+		'World Gray' : L.esri.basemapLayer('Gray', {zIndex : BASE_LAYER_Z_INDEX}),
+		'World Street' : L.tileLayer.provider('Esri.WorldStreetMap', {zIndex : BASE_LAYER_Z_INDEX})
+	};
+
+	var dataLayer = L.coverageLayer(options.layerParams, {
+		opacity: 0.6,
+		zIndex: DATA_LAYER_Z_INDEX
+	});
+
+	var updateLegend = function() {
+		options.$legendImg.attr('src', dataLayer.getLegendGraphicURL());
+	};
+
+	var map = new MapWithSingleClickHandler(options.mapDivId, {
+		center: [37.0, -100.0],
+		zoom: 3,
+		layers: [baseLayers['World Gray'], dataLayer]
+	});
+
+
+	map.addControl(L.control.layers(baseLayers), {}, {
+		autoZIndex : false
+	});
+	updateLegend();
+
+	self.updateDataLayer = function(layerParams) {
+		dataLayer.updateLayerParams(layerParams);
+		updateLegend();
+	};
+
+	return self;
+};
---
+++ wqp/portal_ui/static/js/leafletLayers/CoverageLayer.js
@@ -0,0 +1,107 @@
+/* jslint browser: true */
+/* global L */
+/* global Config */
+
+(function() {
+	"use strict";
+
+	var WMS_VERSION = '1.1.0';
+	var NAMESPACE = 'qw_portal_map';
+
+	var LAYER_NAME = {
+		states: NAMESPACE + ':states_all',
+		counties: NAMESPACE + ':counties_all',
+		huc8: NAMESPACE + ':huc8_all'
+	};
+
+	var VIEWPARAMS_SOURCE = {
+		'storet': 'source1:E;source2:E',
+		'nwis': 'source1:N;source2:N',
+		'all': 'source1:E;source2:N'
+	};
+	var SLD_DATASOURCE = {
+		'storet': 'E',
+		'nwis': 'N',
+		'all': 'A'
+	};
+	var SLD_FEATURE = {
+		'states': 'S',
+		'counties': 'C',
+		'huc8': 'H'
+	};
+	var SLD_TIMEFRAME = {
+		'past_12_months': '1',
+		'past_60_months': '5',
+		'all_time': 'A'
+	};
+	var getLayerName = function(displayBy) {
+		return LAYER_NAME[displayBy];
+	};
+	var getViewParams = function(layerParams) {
+		return VIEWPARAMS_SOURCE[layerParams.dataSource] + ';timeFrame:' + layerParams.timeSpan;
+	};
+	var getSLDURL = function(layerParams) {
+		return Config.SLD_ENDPOINT +
+			'?dataSource=' + SLD_DATASOURCE[layerParams.dataSource] +
+			'&geometry=' + SLD_FEATURE[layerParams.displayBy] +
+			'&timeFrame=' + SLD_TIMEFRAME[layerParams.timeSpan];
+	};
+	var getWMSParams = function(layerParams) {
+		return {
+			layers : getLayerName(layerParams.displayBy),
+			VIEWPARAMS: getViewParams(layerParams),
+			sld: getSLDURL(layerParams)
+		};
+	};
+
+	/*
+	 * @constructs - extends L.TileLayer.WMS
+	 * The url should not be passed into the constructor. The layers, format, transparent, and version options are
+	 * defaulted.
+	 * @param {Object} layerParams -
+	 * 		@prop {String} displayBy - spatial feature
+	 * 		@prop {String} timeSpan - Allowed values: past_12_months, past_60_months, all_time
+	 * 		@prop {String} dataSource - Allowed values: storet, nwis, all.
+	 * @param {Object} options - Can be any L.TileLayer.WMS options
+	 */
+	L.CoverageLayer = L.TileLayer.WMS.extend({
+
+		defaultWmsParams: {
+			VIEWPARAMS : '',
+			layers : '',
+			sld : '',
+			format: 'image/png',
+			version: WMS_VERSION,
+			request : 'GetMap',
+			transparent: true,
+		},
+
+		initialize : function(layerParams, options) {
+			L.TileLayer.WMS.prototype.initialize.call(this, Config.WQP_MAP_GEOSERVER_ENDPOINT + 'wms', options);
+			$.extend(this.wmsParams, getWMSParams(layerParams));
+		},
+
+		updateLayerParams : function(layerParams) {
+			this.setParams(getWMSParams(layerParams));
+		},
+
+		/*
+		 * Returns a url string which can be used to retrieve an legend image that represents the layer.
+		 * @returns {String}
+		 */
+		getLegendGraphicURL : function() {
+			var queryParams = {
+				request : 'GetLegendGraphic',
+				format : 'image/png',
+				layer : this.wmsParams.layers,
+				sld : this.wmsParams.sld,
+				VIEWPARAMS : this.wmsParams.VIEWPARAMS
+			};
+			return Config.WQP_MAP_GEOSERVER_ENDPOINT + 'wms?' + $.param(queryParams);
+		}
+	});
+
+	L.coverageLayer = function(layerParams, options) {
+		return new L.CoverageLayer(layerParams, options);
+	};
+})();
