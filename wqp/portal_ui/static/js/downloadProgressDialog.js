7df9aef5b7d876cf05961223e1d5e8e1e76e3622
Mary Bucknell <mbucknell@usgs.gov>
2016-12-16 12:34:58 -0600

    reformat code

--- wqp/portal_ui/static/js/downloadProgressDialog.js
+++ wqp/portal_ui/static/js/downloadProgressDialog.js
@@ -6,41 +6,41 @@
 var PORTAL = PORTAL || {};
 PORTAL.VIEWS = PORTAL.VIEWS || {};
 
-PORTAL.VIEWS.downloadProgressDialog = function(el) {
+PORTAL.VIEWS.downloadProgressDialog = function (el) {
 	"use strict";
 
-    var that = {};
+	var that = {};
 
-    var totalCountProp = {
-    	'Station' : 'sites',
-		'Result' : 'results',
-		'Activity' : 'activities'
+	var totalCountProp = {
+		'Station': 'sites',
+		'Result': 'results',
+		'Activity': 'activities'
 	};
 
-    // constants for the two different download statuses
-    var DIALOG = {
-        map : {
-            title : 'Map Sites Status',
-            continueMessage : 'map the sites',
-            cancelDownload : function(sitesCount) {
-                var intSiteCount = parseInt(sitesCount.split(',').join(''));
-                return (intSiteCount > 250000);
-            },
-            cancelMessage : 'Your query is returning more than 250,000 sites and can not be mapped.'
-        },
-        download : {
-            title : 'Download Status',
-            continueMessage : 'download the data',
-            cancelDownload : function(counts, fileFormat) {
-				return (counts !== 0) && (fileFormat === 'xlsx') &&  (parseInt(counts.split(',').join('')) > 1048575);
-            },
-            cancelMessage : 'Your query is returning more than 1,048,575 results which exceeds Excel\'s limit.'
-        }
-    };
+	// constants for the two different download statuses
+	var DIALOG = {
+		map: {
+			title: 'Map Sites Status',
+			continueMessage: 'map the sites',
+			cancelDownload: function (sitesCount) {
+				var intSiteCount = parseInt(sitesCount.split(',').join(''));
+				return (intSiteCount > 250000);
+			},
+			cancelMessage: 'Your query is returning more than 250,000 sites and can not be mapped.'
+		},
+		download: {
+			title: 'Download Status',
+			continueMessage: 'download the data',
+			cancelDownload: function (counts, fileFormat) {
+				return (counts !== 0) && (fileFormat === 'xlsx') && (parseInt(counts.split(',').join('')) > 1048575);
+			},
+			cancelMessage: 'Your query is returning more than 1,048,575 results which exceeds Excel\'s limit.'
+		}
+	};
 
-    var opKind;
+	var opKind;
 
-    var countsHbTemplate = Handlebars.compile('Your query will return ' +
+	var countsHbTemplate = Handlebars.compile('Your query will return ' +
 		'{{#if total.results}} <b>{{total.results}}</b> sample results from {{/if}}' +
 		'{{#if total.activities }} <b>{{total.activities}}</b> activity results from {{/if}}' +
 		' <b>{{total.sites}}</b> sites:<br />' +
@@ -51,41 +51,41 @@ PORTAL.VIEWS.downloadProgressDialog = function(el) {
 		'{{/each}}'
 	);
 
-    var buttonHtml = function(id, label) {
-        return '<button id="' + id + '" type="button" class="btn btn-default">' + label + '</button>';
-    };
+	var buttonHtml = function (id, label) {
+		return '<button id="' + id + '" type="button" class="btn btn-default">' + label + '</button>';
+	};
 
-    that.show = function(thisOpKind, dialogMessage) {
+	that.show = function (thisOpKind, dialogMessage) {
 		var message = (dialogMessage) ? dialogMessage : 'Validating query ... Please wait.';
-        opKind = thisOpKind;
-        
-        el.find('.modal-footer').html('');
-        el.find('.modal-body').html(message);
-        el.find('.modal-header h4').html(DIALOG[opKind].title);
-        el.modal('show');
-    };
+		opKind = thisOpKind;
 
-    that.hide = function() {
-        el.modal('hide');
-    };
-    
-	that.updateProgress = function(counts, resultType, fileFormat, continueFnc) {
+		el.find('.modal-footer').html('');
+		el.find('.modal-body').html(message);
+		el.find('.modal-header h4').html(DIALOG[opKind].title);
+		el.modal('show');
+	};
+
+	that.hide = function () {
+		el.modal('hide');
+	};
+
+	that.updateProgress = function (counts, resultType, fileFormat, continueFnc) {
 		var totalCount = counts.total[totalCountProp[resultType]];
 
-		var getCountMessage = function(){
+		var getCountMessage = function () {
 			// Return a string showing the site counts, formatted to be shown in html.
 			var context = {
-				total : counts.total
+				total: counts.total
 			};
-			context.providers = _.map(PORTAL.MODELS.providers.getIds(), function(provider) {
+			context.providers = _.map(PORTAL.MODELS.providers.getIds(), function (provider) {
 				return {
-					id : provider,
-					counts : counts[provider]
+					id: provider,
+					counts: counts[provider]
 				};
 			});
 			return countsHbTemplate(context);
 		};
-		
+
 
 		if (totalCount === '0') {
 			that.cancelProgress('Your query returned no data to download.');
@@ -93,30 +93,30 @@ PORTAL.VIEWS.downloadProgressDialog = function(el) {
 		else if (DIALOG[opKind].cancelDownload(totalCount, fileFormat)) {
 			that.cancelProgress(getCountMessage() + DIALOG[opKind].cancelMessage);
 		}
-		
+
 		else {
 			el.find('.modal-body').html(getCountMessage() + '<p>Click Continue to ' + DIALOG[opKind].continueMessage);
-            el.find('.modal-footer').html(buttonHtml('progress-cancel-btn', 'Cancel') +
-                    buttonHtml('progress-continue-btn', 'Continue'));
-            $('#progress-cancel-btn').click(function() {
-                el.modal('hide');
-            });
-            $('#progress-continue-btn').click(function() {
-                el.modal('hide');
-                continueFnc(totalCount);
-            });			
+			el.find('.modal-footer').html(buttonHtml('progress-cancel-btn', 'Cancel') +
+				buttonHtml('progress-continue-btn', 'Continue'));
+			$('#progress-cancel-btn').click(function () {
+				el.modal('hide');
+			});
+			$('#progress-continue-btn').click(function () {
+				el.modal('hide');
+				continueFnc(totalCount);
+			});
 		}
 	};
-    
-	that.cancelProgress = function(message) {
+
+	that.cancelProgress = function (message) {
 		el.find('.modal-body').html(message);
 		el.find('.modal-footer').html(buttonHtml('progress-ok-btn', 'Ok'));
-		$('#progress-ok-btn').click(function() {
+		$('#progress-ok-btn').click(function () {
 			el.modal('hide');
 		});
 	};
 
-    return that;
+	return that;
 
 };
 
