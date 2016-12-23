6d03f2958792f91f14dfc2048bebe0a736cf6357
Yan, Andrew N <ayan@usgs.gov>
2016-12-23 09:03:10 -0600

    add tests for sites maps and nldi calls

---
+++ test/js/portal_ui/providerSitesMapSpec.js
@@ -0,0 +1,9 @@
+/* jslint browser: true */
+/* global describe, beforeEach, afterEach, it, expect, spyOn, jasmine */
+/* global $ */
+/* global Config */
+/* global L */
+/* global SITE */
+
+
+describe
