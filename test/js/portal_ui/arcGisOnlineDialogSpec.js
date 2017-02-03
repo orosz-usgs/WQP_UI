110a589b3dac5e5aa91607aedb731dc6525280fd
Andrew Yan <ayan@usgs.gov>
2017-02-03 15:57:26 -0600

    start tests for new views

---
+++ test/js/portal_ui/arcGisOnlineDialogSpec.js
@@ -0,0 +1,7 @@
+/* jslint browser: true */
+/* global describe, beforeEach, afterEach, it, expect, jasmine, spyOn */
+/* global $ */
+/* global PORTAL */
+
+describe('Tests for PORTAL.Views.arcGisOnlineDialog', function() {
+});
---
+++ test/js/portal_ui/views/arcGisOnlineHelpViewSpec.js
@@ -0,0 +1,7 @@
+/* jslint browser: true */
+/* global describe, beforeEach, afterEach, it, expect, spyOn, jasmine */
+/* global $ */
+/* global PORTAL */
+
+describe ('Tests for PORTAL.VIEWS.arcGisOnlineHelp', function() {
+});
