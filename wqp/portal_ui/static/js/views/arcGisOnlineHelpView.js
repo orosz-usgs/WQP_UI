cfe950abc4a2f7883624836539178f20207fb94e
Mary Bucknell <mbucknell@usgs.gov>
2017-02-08 15:28:18 -0600

    Moved the loading of the ARC GIS help template outside of the call to instantiate the module so that it only gets loaded once and it is done immediately on loading the javascript file.

--- wqp/portal_ui/static/js/views/arcGisOnlineHelpView.js
+++ wqp/portal_ui/static/js/views/arcGisOnlineHelpView.js
@@ -8,20 +8,9 @@
 var PORTAL = PORTAL || {};
 PORTAL.VIEWS = PORTAL.VIEWS || {};
 
-/*
- * @param {Jquery element} $button - Arc GIS help button
- * @param {Jquery element} $dialog - Arc GIS help dialog
- * @param {Jquery element} $siteMapViewContainer - The map view container element
- * @param {Function} getQueryParamArray -
- * 		@returns {Array of Objects with name and value properties} - The form's current query parameters.
- */
-PORTAL.VIEWS.arcGisOnlineHelpView = function(options) {
+(function() {
 	"use strict";
-
-	var self = {};
-
-	var HEADER = "Using WQP Maps with ArcGIS online";
-
+	/* Need to preload the dialog. */
 	var NULL_TEMPLATE = function() {
 		return 'Template has not been loaded';
 	};
@@ -29,56 +18,70 @@ PORTAL.VIEWS.arcGisOnlineHelpView = function(options) {
 	var dialogBodyTemplate =  NULL_TEMPLATE;
 
 	/*
-	 * Shows the Arc GIS help dialog with the content reflecting the parameters.
-	 * @param {Array of Objects with name and value properties} - Represents the query parameters that
-	 * 		will be in the dialog
-	 * @param {String} selectedSld - The SLD string that will be used to in the dialog
-	 */
-	var showDialog = function(queryParams, selectedSld) {
-		var wfsUrl = decodeURIComponent(L.WQPSitesLayer.getWfsGetFeatureUrl(queryParams));
-		var queryStr = wfsUrl.substring(wfsUrl.indexOf('?') + 1);
-		var queryStrPairs = queryStr.split('&');
-		var parameters = {};
-		queryStrPairs.forEach(function(pair) {
-			pair = pair.split('=');
-			parameters[pair[0]] = pair[1];
-		});
-		var hbContext = {
-			searchParams : parameters.SEARCHPARAMS,
-			style : selectedSld
-		};
-
-		options.$dialog.find('.modal-body').html(dialogBodyTemplate(hbContext));
-		options.$dialog.modal('show');
-	};
-
-	/*
 	 * Read handlebar template
 	 */
 	$.ajax({
-		url : Config.STATIC_ENDPOINT + 'js/hbTemplates/arcGisHelp.hbs',
+		url: Config.STATIC_ENDPOINT + 'js/hbTemplates/arcGisHelp.hbs',
 		cache: false,
-		success : function(response) {
+		success: function (response) {
 			dialogBodyTemplate = Handlebars.compile(response);
 		},
-		error : function( ) {
+		error: function () {
 			log.error('Unable to read template hbTemplates/nldiFeatureSourcePopup.hbs');
 		}
 	});
-
 	/*
-	 * Initialize the Arc GIS online view, initializing content and setting up event handlers as needed.
+	 * @param {Jquery element} $button - Arc GIS help button
+	 * @param {Jquery element} $dialog - Arc GIS help dialog
+	 * @param {Jquery element} $siteMapViewContainer - The map view container element
+	 * @param {Function} getQueryParamArray -
+	 * 		@returns {Array of Objects with name and value properties} - The form's current query parameters.
 	 */
-	self.initialize = function() {
-		var $sldSelect = options.$siteMapViewContainer.find('#sld-select-input');
+	PORTAL.VIEWS.arcGisOnlineHelpView = function (options) {
+		"use strict";
 
-		options.$dialog.find('.modal-header h4').html(HEADER);
+		var self = {};
 
-		options.$button.click(function() {
-			var queryParamArray = options.getQueryParamArray();
-			var selectedSld = $sldSelect.val();
-			showDialog(queryParamArray, selectedSld);
-		});
+		var HEADER = "Using WQP Maps with ArcGIS online";
+
+		/*
+		 * Shows the Arc GIS help dialog with the content reflecting the parameters.
+		 * @param {Array of Objects with name and value properties} - Represents the query parameters that
+		 * 		will be in the dialog
+		 * @param {String} selectedSld - The SLD string that will be used to in the dialog
+		 */
+		var showDialog = function (queryParams, selectedSld) {
+			var wfsUrl = decodeURIComponent(L.WQPSitesLayer.getWfsGetFeatureUrl(queryParams));
+			var queryStr = wfsUrl.substring(wfsUrl.indexOf('?') + 1);
+			var queryStrPairs = queryStr.split('&');
+			var parameters = {};
+			queryStrPairs.forEach(function (pair) {
+				pair = pair.split('=');
+				parameters[pair[0]] = pair[1];
+			});
+			var hbContext = {
+				searchParams: parameters.SEARCHPARAMS,
+				style: selectedSld
+			};
+
+			options.$dialog.find('.modal-body').html(dialogBodyTemplate(hbContext));
+			options.$dialog.modal('show');
+		};
+
+		/*
+		 * Initialize the Arc GIS online view, initializing content and setting up event handlers as needed.
+		 */
+		self.initialize = function () {
+			var $sldSelect = options.$siteMapViewContainer.find('#sld-select-input');
+
+			options.$dialog.find('.modal-header h4').html(HEADER);
+
+			options.$button.click(function () {
+				var queryParamArray = options.getQueryParamArray();
+				var selectedSld = $sldSelect.val();
+				showDialog(queryParamArray, selectedSld);
+			});
+		};
+		return self;
 	};
-	return self;
-};
+})();
