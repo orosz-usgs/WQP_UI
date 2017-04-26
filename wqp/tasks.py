0dca2cb41323e4777004b38f2e661fddba58a210
Andrew Yan <ayan@usgs.gov>
2017-04-26 08:21:35 -0500

    improve logging in tasks

--- wqp/tasks.py
+++ wqp/tasks.py
@@ -21,8 +21,7 @@ def load_sites_into_cache_async(self, provider_id):
     :param provider_id: the identifier of the provider (NWIS, STORET, ETC)
     :return: dict - with keys for status (code of request for sites), cached_count, error_count, and total_count
     """
-    logger.debug('Test message.')
-    # app.logger.debug('Starting async load of sites into Redis cache.')
+    logger.debug('Starting async load of sites into Redis cache.')
     search_endpoint = app.config['SEARCH_QUERY_ENDPOINT'] + "Station/search/"
     redis_config = app.config['REDIS_CONFIG']
     result = {'status': '',
@@ -34,7 +33,7 @@ def load_sites_into_cache_async(self, provider_id):
     if redis_config:
         db_number = generate_redis_db_number(provider_id)
         redis_msg = create_redis_log_msg(redis_config['host'], redis_config['port'], db_number)
-        app.logger.info(redis_msg)
+        logger.info(redis_msg)
         redis_session = redis.StrictRedis(host=redis_config['host'],
                                           port=redis_config['port'],
                                           db=db_number,
@@ -46,7 +45,8 @@ def load_sites_into_cache_async(self, provider_id):
                                                     },
                            stream=True
                            )
-
+        resp_log_msg = create_request_resp_log_msg(resp)
+        logger.debug(resp_log_msg)
         result['status'] = resp.status_code
         if resp.status_code == 200:
             result['total_count'] = int(resp.headers['Total-Site-Count'])
@@ -66,10 +66,6 @@ def load_sites_into_cache_async(self, provider_id):
                                         'total': result['total_count'],
                                         'status': 'working'}
                                   )
-        else:
-            msg = create_request_resp_log_msg(resp)
-            # app.logger.warning(msg)
-
         # Add loading stats to cache
         status_key = provider_id + '_sites_load_status'
         status_content = {'time_utc': arrow.utcnow(),
@@ -80,7 +76,7 @@ def load_sites_into_cache_async(self, provider_id):
         redis_session.set(status_key, pickle.dumps(status_content))
 
     else:
-        # app.logger.warning('Redis has not been configured.')
+        logger.warning('Redis has not been configured.')
         self.update_state(state='NO_REDIS_CONFIGURED', meta={})
 
     return result
