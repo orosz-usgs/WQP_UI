06f63dc56bf820cb2e4611610080e59988fec567
Daniel Naab <dnaab@usgs.gov>
2018-06-08 10:59:38 -0500

    Remove globals from nldiMapping.js

--- assets/js/providerSiteMapOnReady.js
+++ assets/js/providerSiteMapOnReady.js
@@ -1,7 +1,10 @@
+import addOverlays from './nldiMapping';
+
+
 $(document).ready(function() {
     var site = Config.site;
     var latitude = site.LatitudeMeasure;
     var longitude = site.LongitudeMeasure;
     var map = window.SITE.siteMap(latitude, longitude, {mapDivId : 'sites_map', mapZoom: 10});
-    NLDI.addOverlays(map);
+    addOverlays(map);
 });
