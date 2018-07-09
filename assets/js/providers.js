946345c415033da155a1128049ab24434723c2b0
Mary Bucknell <mbucknell@usgs.gov>
2018-07-09 15:03:31 -0500

    Linting

--- assets/js/providers.js
+++ assets/js/providers.js
@@ -30,7 +30,6 @@ class Providers {
             },
             error: (jqXHR, textStatus, error) => {
                 this.ids = [];
-                let msg = '';
                 log.error('Unable to retrieve provider list with error: ' + error);
                 deferred.reject(jqXHR, error);
             }
