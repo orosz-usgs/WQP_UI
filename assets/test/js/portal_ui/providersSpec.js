e2d7cbbc9513d9a4aa0ee010020cb9a53c75a99f
Mary Bucknell <mbucknell@usgs.gov>
2019-01-28 15:43:50 -0600

    Update jasmine version. Fixed providersSpect test.

--- assets/test/js/portal_ui/providersSpec.js
+++ assets/test/js/portal_ui/providersSpec.js
@@ -4,74 +4,78 @@ import providers from '../../../js/providers';
 
 
 describe('Tests for providers', function () {
-    var server;
-    var successSpy, failureSpy;
 
-    var RESPONSE = '{"codes" : [{"value": "Src1"}, {"value" : "Src2"}, {"value" : "Src3"}]}';
-
-    beforeEach(function () {
-        server = sinon.fakeServer.create();
-
-        successSpy = jasmine.createSpy('successSpy');
-        failureSpy = jasmine.createSpy('failureSpy');
-        spyOn(log, 'error');
+    describe('State prior to fetching', () => {
+        it('Should return an empty array of ids if initialize has not been called', function() {
+            expect(providers.getIds()).toEqual([]);
+        });
     });
 
-    afterEach(function () {
-        server.restore();
-    });
+    describe('Test fetch and getIds', () => {
+        let server;
+        var successSpy, failureSpy;
 
-    it('Should return an empty array of ids if initialize has not been called', function () {
-        expect(providers.getIds()).toEqual([]);
-    });
+        var RESPONSE = '{"codes" : [{"value": "Src1"}, {"value" : "Src2"}, {"value" : "Src3"}]}';
 
-    it('Should call ajax to get data when fetch function is invoked', function () {
-        providers.fetch();
-        expect(server.requests.length).toBe(1);
-        expect(server.requests[0].url).toContain(Config.CODES_ENDPOINT + '/providers');
-    });
+        beforeEach(function() {
+            server = sinon.fakeServer.create();
 
-    it('When fetch is called,initialize the ids and call successFnc when a successful ajax call is made', function () {
-        providers.fetch().done(successSpy).fail(failureSpy);
+            successSpy = jasmine.createSpy('successSpy');
+            failureSpy = jasmine.createSpy('failureSpy');
+            spyOn(log, 'error');
+        });
 
-        server.requests[0].respond(200, {'Content-Type': 'text/json'}, RESPONSE);
+        afterEach(function() {
+            server.restore();
+        });
 
-        expect(successSpy).toHaveBeenCalled();
-        expect(failureSpy).not.toHaveBeenCalled();
-        expect(providers.getIds()).toEqual(['Src1', 'Src2', 'Src3']);
-    });
-
-    it('Should call failureFnc when an unsuccessful fetch is made.', function () {
-        providers.fetch().done(successSpy).fail(failureSpy);
-        server.requests[0].respond(500, {}, 'Bad data');
-
-        expect(failureSpy).toHaveBeenCalled();
-        expect(successSpy).not.toHaveBeenCalled();
-        expect(providers.getIds()).toEqual([]);
-    });
-
-    describe('Tests formatAvailableProviders', function () {
-
-        beforeEach(function () {
+        it('Should call ajax to get data when fetch function is invoked', function() {
             providers.fetch();
+            expect(server.requests.length).toBe(1);
+            expect(server.requests[0].url).toContain(Config.CODES_ENDPOINT + '/providers');
+        });
+
+        it('When fetch is called,initialize the ids and call successFnc when a successful ajax call is made', function() {
+            providers.fetch().done(successSpy).fail(failureSpy);
+
             server.requests[0].respond(200, {'Content-Type': 'text/json'}, RESPONSE);
+
+            expect(successSpy).toHaveBeenCalled();
+            expect(failureSpy).not.toHaveBeenCalled();
+            expect(providers.getIds()).toEqual(['Src1', 'Src2', 'Src3']);
         });
 
-        it('Expects "all" to be returned if all available providers are specified', function () {
-            expect(providers.formatAvailableProviders('Src1 Src2 Src3')).toEqual('all');
+        it('Should call failureFnc when an unsuccessful fetch is made.', function() {
+            providers.fetch().done(successSpy).fail(failureSpy);
+            server.requests[0].respond(500, {}, 'Bad data');
+
+            expect(failureSpy).toHaveBeenCalled();
+            expect(successSpy).not.toHaveBeenCalled();
+            expect(providers.getIds()).toEqual([]);
         });
 
-        it('Expects a comma separated list of providers returned if only some are specified', function () {
-            expect(providers.formatAvailableProviders('Src2 Src3')).toEqual('Src2, Src3');
-            expect(providers.formatAvailableProviders('Src2')).toEqual('Src2');
-        });
+        describe('Tests formatAvailableProviders', function() {
 
-        it('Expects providers that are not in the initialized to be eliminated', function () {
-            expect(providers.formatAvailableProviders('Src1 Src2 Src3 Src4')).toEqual('all');
-            expect(providers.formatAvailableProviders('Src1 Src3 Src4')).toEqual('Src1, Src3');
-            expect(providers.formatAvailableProviders('Src4')).toEqual('');
-        });
+            beforeEach(function() {
+                providers.fetch();
+                server.requests[0].respond(200, {'Content-Type': 'text/json'}, RESPONSE);
+            });
 
+            it('Expects "all" to be returned if all available providers are specified', function() {
+                expect(providers.formatAvailableProviders('Src1 Src2 Src3')).toEqual('all');
+            });
+
+            it('Expects a comma separated list of providers returned if only some are specified', function() {
+                expect(providers.formatAvailableProviders('Src2 Src3')).toEqual('Src2, Src3');
+                expect(providers.formatAvailableProviders('Src2')).toEqual('Src2');
+            });
+
+            it('Expects providers that are not in the initialized to be eliminated', function() {
+                expect(providers.formatAvailableProviders('Src1 Src2 Src3 Src4')).toEqual('all');
+                expect(providers.formatAvailableProviders('Src1 Src3 Src4')).toEqual('Src1, Src3');
+                expect(providers.formatAvailableProviders('Src4')).toEqual('');
+            });
+        });
     });
 });
 
